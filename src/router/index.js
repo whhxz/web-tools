26ec28b92b9bcc48eb5dfddb841ff452ad23c551
whhxz <xuzhuowhh@gmail.com>
2024-10-30 10:48:28 +0800

    添加首页转跳

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import { nextTick } from 'vue';
 
 const routes = [
   {
+    path: '/',
+    code: 'index',
+    name: '首页',
+    component: () => import('@/view/index/index.vue'),
+  },
+  {
     path: '/week',
     code: 'week',
     name: '周',
---
+++ src/view/index/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <ul>
+    <li><RouterLink to="/week">周</RouterLink></li>
+    <li><RouterLink to="/temperature">体温记录</RouterLink></li>
+  </ul>
+</template>
